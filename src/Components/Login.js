f9a866f63eb0de879087be3ba3daff448c95e118
Mentallsoft <efrain.pizarro@uao.edu.co>
2019-11-12 14:56:00 -0500

    Se eliminan objetos innescesarios y se carga la imagen de fondo

---
+++ src/Components/Login.js
@@ -0,0 +1,17 @@
+import React from "react"
+import "./CSS/Login.css"
+
+class Login extends React.Component {
+
+
+    render() {
+        return (
+            <div className="o-background">hola</div> 
+            )
+    }
+
+
+
+}
+
+export default Login;
